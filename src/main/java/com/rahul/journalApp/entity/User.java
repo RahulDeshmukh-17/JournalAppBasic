cbe19a6ad1d29b4edbc24c11aa5799a2702e218b
Rahul <deshmukhrahul636@gmail.com>
2025-03-16 10:06:29 +0530

    adding user

--- src/main/java/com/rahul/journalApp/entity/User.java
+++ src/main/java/com/rahul/journalApp/entity/User.java
@@ -11,6 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private ObjectId Id;
+
     @Indexed(unique = true)
     private String username;
     private String password;
